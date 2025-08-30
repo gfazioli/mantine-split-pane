d7a01ea05aaa6a146d07933ec846c9eba60ad0b6
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2025-08-30 12:30:16 +0200

    📦 Updates packages to the latest version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
 import mantine from 'eslint-config-mantine';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(...mantine, { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}'] });
+export default tseslint.config(...mantine, { ignores: ['**/.next/**', '**/*.{mjs,cjs,js,d.ts,d.mts}'] });
