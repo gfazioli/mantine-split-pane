ae1a872b40c6656c0232ab91c2e40228daaef727
Pirulax <patrikjankovics7@gmail.com>
2026-02-18 00:41:06 +0100

    Fix

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,15 +1,15 @@
-export { useSplitContext } from './Split.context';
-
 export { Split } from './Split';
 export type { SplitCssVariables, SplitFactory, SplitProps } from './Split';
 
-export type { SPLIT_PANE_SIZE, SplitPane, SplitPaneFactory, SplitPaneProps } from './Pane/SplitPane';
+export { SplitPane } from './Pane/SplitPane';
+export type { SPLIT_PANE_SIZE, SplitPaneFactory, SplitPaneProps } from './Pane/SplitPane';
 
+export { SplitResizer } from './Resizer/SplitResizer';
 export type {
   SPLIT_PANE_RESIZE_SIZES,
-  SplitResizer,
   SplitResizerFactory,
   SplitResizerProps,
 } from './Resizer/SplitResizer';
 
-export type { PaneConfig, SplitDynamic, SplitDynamicProps } from './Dynamic';
+export { SplitDynamic } from './Dynamic';
+export type { PaneConfig, SplitDynamicProps } from './Dynamic';
