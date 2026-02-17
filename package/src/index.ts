1780374c77fc54af1bf5cf20db0fa05489336b50
Pirulax <patrikjankovics7@gmail.com>
2026-02-18 00:37:41 +0100

    Export `SplitDynamic`, `SplitResizer`, `SplitPane` directly
    Compound components cannot be used in server components - fix is to export the components directly.
    See https://mantine.dev/guides/next/#compound-components-in-server-components

--- package/src/index.ts
+++ package/src/index.ts
@@ -3,12 +3,13 @@ export { useSplitContext } from './Split.context';
 export { Split } from './Split';
 export type { SplitCssVariables, SplitFactory, SplitProps } from './Split';
 
-export type { SPLIT_PANE_SIZE, SplitPaneFactory, SplitPaneProps } from './Pane/SplitPane';
+export type { SPLIT_PANE_SIZE, SplitPane, SplitPaneFactory, SplitPaneProps } from './Pane/SplitPane';
 
 export type {
   SPLIT_PANE_RESIZE_SIZES,
+  SplitResizer
   SplitResizerFactory,
   SplitResizerProps,
 } from './Resizer/SplitResizer';
 
-export type { PaneConfig, SplitDynamicProps } from './Dynamic';
+export type { PaneConfig, SplitDynamic, SplitDynamicProps } from './Dynamic';
